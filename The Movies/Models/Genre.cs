90e68eee212b4ac52c9a05c8dc31ff04e83a3a0b
benjiens1985 <jens9226@stud.kea.dk>
2026-08-24 10:43:34 +0200

    Tilføj mappestruktur, ViewModelBase, RelayCommand, Movie og Genre

---
+++ The Movies/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿namespace The_Movies.Models
+{
+    public enum Genre
+    {
+        Action,
+        Comedy,
+        Drama,
+        Horror,
+        SciFi,
+        Thriller,
+        Romance,
+        Documentary,
+        Animation,
+        Fantasy
+    }
+}
---
+++ The Movies/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace The_Movies.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; } = string.Empty;
+        public int Duration { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
