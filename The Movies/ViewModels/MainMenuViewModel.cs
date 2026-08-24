17b727e859d4d6b939d6c75a9bc92162b2ae83a1
benjiens1985 <jens9226@stud.kea.dk>
2026-08-24 12:25:32 +0200

    Tilføj MainMenuViewModel med kommandoer

---
+++ The Movies/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using TheMovies.ViewModels;
+
+namespace The_Movies.ViewModels
+{
+    public class MainMenuViewModel : ViewModelBase
+    {
+        public ICommand RegisterMovieCommand { get; }
+        public ICommand ShowMovieListCommand { get; }
+
+        public MainMenuViewModel()
+        {
+            RegisterMovieCommand = new RelayCommand(_ => RegisterMovie());
+            ShowMovieListCommand = new RelayCommand(_ => ShowMovieList());
+        }
+
+        private void RegisterMovie()
+        {
+            // Skal senere skifte "aktiv side" til registreringsformularen
+        }
+
+        private void ShowMovieList()
+        {
+            // Skal senere skifte "aktiv side" til filmoversigten
+        }
+    }
+}
