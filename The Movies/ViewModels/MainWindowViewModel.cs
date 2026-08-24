ccdb84d72ebbcbfd0557145acda8b4a353636da6
benjiens1985 <jens9226@stud.kea.dk>
2026-08-24 12:49:36 +0200

    Tilføj MainWindowViewModel til navigationsstyring v2

---
+++ The Movies/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace The_Movies.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private object? _currentView;
+        public object? CurrentView
+        {
+            get => _currentView;
+            set => SetProperty(ref _currentView, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            CurrentView = new MainMenuViewModel();
+        }
+    }
+}
