b2ff3ef15e294e60635ed98e22f0c5719ec8c5f8
benjiens1985 <jens9226@stud.kea.dk>
2026-08-26 12:46:35 +0200

    Unittest tilføjet og kørt uden fejl.

---
+++ TheMovies.Tests/RegisterMovieViewModelTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using The_Movies.ViewModels;
+
+namespace TheMovies.Tests
+{
+    [TestClass]
+    public class RegisterMovieViewModelTests
+    {
+        [TestMethod]
+        public void CanSave_ReturnsFalse_WhenTitleIsEmpty()
+        {
+            var mainWindowViewModel = new MainWindowViewModel();
+            var viewModel = new RegisterMovieViewModel(mainWindowViewModel);
+
+            viewModel.Title = "";
+            viewModel.Duration = "90";
+
+            bool canSave = viewModel.SaveCommand.CanExecute(null);
+
+            Assert.IsFalse(canSave);
+        }
+
+        [TestMethod]
+        public void CanSave_ReturnsFalse_WhenDurationIsNotANumber()
+        {
+            var mainWindowViewModel = new MainWindowViewModel();
+            var viewModel = new RegisterMovieViewModel(mainWindowViewModel);
+
+            viewModel.Title = "Test Film";
+            viewModel.Duration = "abc";
+
+            bool canSave = viewModel.SaveCommand.CanExecute(null);
+
+            Assert.IsFalse(canSave);
+        }
+
+        [TestMethod]
+        public void CanSave_ReturnsTrue_WhenTitleAndDurationAreValid()
+        {
+            var mainWindowViewModel = new MainWindowViewModel();
+            var viewModel = new RegisterMovieViewModel(mainWindowViewModel);
+
+            viewModel.Title = "Test Film";
+            viewModel.Duration = "90";
+
+            bool canSave = viewModel.SaveCommand.CanExecute(null);
+
+            Assert.IsTrue(canSave);
+        }
+    }
+}
