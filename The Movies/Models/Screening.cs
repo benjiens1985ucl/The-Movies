d607fb522af0e864f743570818d12e9adc3c10d9
benjiens1985 <jens9226@stud.kea.dk>
2026-08-27 14:55:12 +0200

    Tilføj ScreeningRepository

---
+++ The Movies/Models/Screening.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace The_Movies.Models
+{
+    public class Screening
+    {
+        public Movie Movie { get; set; } = new Movie();
+        public Hall Hall { get; set; } = new Hall();
+        public DateTime DateTime { get; set; }
+        public bool IsPremiere { get; set; }
+    }
+}
---
+++ The Movies/Services/ScreeningRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using The_Movies.Models;
+
+namespace The_Movies.Services
+{
+    public class ScreeningRepository
+    {
+        private readonly string _filePath = "screenings.json";
+
+        public void Save(List<Screening> screenings)
+        {
+            string json = JsonSerializer.Serialize(screenings);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public List<Screening> LoadAll()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Screening>();
+            }
+
+            string json = File.ReadAllText(_filePath);
+            var screenings = JsonSerializer.Deserialize<List<Screening>>(json);
+
+            return screenings ?? new List<Screening>();
+        }
+    }
+}
