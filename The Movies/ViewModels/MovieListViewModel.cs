2f81f083b43d80b2722eca9623ef179b5478bf45
VehementWhyrm <emmerencevml@outlook.com>
2026-08-31 21:19:15 +0200

    Adds Slet Film button - tested with a dummy film, works fine.

--- The Movies/ViewModels/MovieListViewModel.cs
+++ The Movies/ViewModels/MovieListViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Input;
 using The_Movies.Models;
 using The_Movies.Services;
@@ -12,7 +13,10 @@ namespace The_Movies.ViewModels
 
         public ObservableCollection<Movie> Movies { get; }
 
+        public Movie? SelectedMovie { get; set; }
+
         public ICommand BackCommand { get; }
+        public ICommand DeleteMovieCommand { get; }
 
         public MovieListViewModel(MainWindowViewModel mainWindowViewModel)
         {
@@ -22,6 +26,37 @@ namespace The_Movies.ViewModels
             Movies = new ObservableCollection<Movie>(loadedMovies);
 
             BackCommand = new RelayCommand(_ => Back());
+            DeleteMovieCommand = new RelayCommand(_ => DeleteMovie());
+        }
+
+        private void DeleteMovie()
+        {
+            if (SelectedMovie == null)
+            {
+                MessageBox.Show(
+                    "Vaelg den film du oensker at slette.",
+                    "Ingen film valgt.",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+
+                return;
+            }
+
+            var result = MessageBox.Show(
+                $"Er du sikker paa, at du vil slette \"{SelectedMovie.Title}\"?",
+                "Slet film",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+
+            if (result != MessageBoxResult.Yes)
+            {
+                return;
+            }
+
+            Movies.Remove(SelectedMovie);
+            _movieRepository.Save(new List<Movie>(Movies));
+
+            SelectedMovie = null;
         }
 
         private void Back()
